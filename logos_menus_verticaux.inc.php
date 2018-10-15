a8f67c726caab1471f808e3ea284475496835713
juliepierson <julie.pierson@cnrs.fr>
2018-10-15 10:55:52 +0200

    adding ouvrir logo

--- logos_menus_verticaux.inc.php
+++ logos_menus_verticaux.inc.php
@@ -1,5 +1,9 @@
+<a href="http://ouvrir.passages.cnrs.fr/" >
+	<img class="logo" src="illustrations/tous/logo_ouvrir.png" alt="logo ouvrir" height="38">
+</a>
+<hspace>
 <a href="http://www.passages.cnrs.fr/" >
-	<img class="logo" src="illustrations/tous/logo_passages.png" alt="logo passages" height="60">
+	<img class="logo" src="illustrations/tous/logo_passages.png" alt="logo passages" height="35">
 </a>
 <!--<a target="_blank" href="http://www.qgis.org/" >
 	<img class="logo" src="illustrations/tous/logo_qgis.png" alt="logo QGIS" height="60">
