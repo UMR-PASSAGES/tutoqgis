b86f6371ddd943fb6c5167d856643754571ce185
juliepierson <julie.pierson@cnrs.fr>
2018-10-15 11:43:50 +0200

    updating about page

--- en_savoir_plus.php
+++ en_savoir_plus.php
@@ -13,13 +13,19 @@
 	
 		<div class="main">
 			<h2>En savoir plus</h2>
-			<p>Ce tutoriel est produit par le pôle Analyse et Représentation des Données de l'<a href="http://www.passages.cnrs.fr">UMR PASSAGES</a>.</p>
-			<p>Il est sous licence Creative Commons : vous êtes autorisé à le partager et l'adapter, pour toute utilisation y compris commerciale, à condition de citer les auteurs : pôle ARD, UMR 5319 PASSAGES, <a href="http://www.passages.cnrs.fr">www.passages.cnrs.fr</a></p>
-			<p>Le texte complet de la licence est disponible ici : <a class="ext" target="_blank" href="http://creativecommons.org/licenses/by/4.0/legalcode">http://creativecommons.org/licenses/by/4.0/legalcode</a></p>
-			<p>Pour nous contacter : envoyez un courriel à l'adresse <b>julie.pierson (at) cnrs.fr</b></p>
-			<p>Faites-nous part de vos remarques, vos idées d'amélioration, ou simplement de l'usage que vous faites de notre tutoriel !</p>
-			<p>L'image du bandeau est tirée des archives de l'art américain et est disponible sur <a class="ext" target="_blank" href="http://commons.wikimedia.org/wiki/File:archives_of_American_Art_-_Guy_Maccoy_-_2905.jpg" >Wikimedia</a>. Cette image est dans le domaine public.</p>
-			<p>Tout le tutoriel est disponible sur GitHub : <a class="ext" target="_blank" href="https://github.com/UMR-PASSAGES/tutoqgis" >https://github.com/UMR-PASSAGES/tutoqgis</a></p>
+			
+			<p>Ce tutoriel est réalisé par Julie Pierson du pôle Analyse et Représentation des Données de l'<a href="http://www.passages.cnrs.fr">UMR PASSAGES</a>. Il côtoie d'autres passionnants tutoriels sur la plate-forme <a href="http://ouvrir.passages.cnrs.fr">OUVRIR</a>.</p>
+			<p>Il a bénéficié de relectures et conseils avisés de (par ordre alphabétique) : Xavier Amelot, Véronique André-Lamat, Gaëlle Deletraz, Marina Duféal, Grégoire Le Campion, Delphine Montagne, Guilhem Mousselin, Matthieu Noucher, Olivier Pissoat, Raphaël Schirmer. Merci à eux !</p>
+			
+			<p>Ce tutoriel est diffusé sous <b>licence Creative Commons</b> : vous êtes autorisé à le partager et l'adapter, pour toute utilisation y compris commerciale, à condition de citer l'auteur (<em>Julie Pierson, UMR 5319 PASSAGES, www.passages.cnrs.fr</em>), et de redistribuer votre travail sous les mêmes conditions. Le texte complet est disponible ici : <a class="ext" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">http://creativecommons.org/licenses/by/4.0/legalcode</a></p>
+			<p>Cela signifie donc que vous pouvez en faire ce que bon vous semble, y compris l'utiliser pour vos propres formations. Et dans ce cas, un petit message nous fait plaisir et nous encourage à continuer ! Faites-nous part également de vos remarques, critiques et idées d'amélioration !</p>
+
+			<p><b>Pour tout contact :</b> envoyez un courriel à julie.pierson (at) cnrs.fr, ou bien <a class="ext" target="_blank" href="https://twitter.com/sigomatique">@sigomatique</a> sur Twitter.</p>
+			
+			<p>Enfin, tout le code du tutoriel (HTML, CSS...) est <b>disponible sur GitHub</b> : <a class="ext" target="_blank" href="https://github.com/UMR-PASSAGES/tutoqgis" >https://github.com/UMR-PASSAGES/tutoqgis</a></p>
+			
+			<p><em>L'image du bandeau est tirée des archives de l'art américain et est disponible sur <a class="ext" target="_blank" href="http://commons.wikimedia.org/wiki/File:archives_of_American_Art_-_Guy_Maccoy_-_2905.jpg" >Wikimedia</a>. Cette image est dans le domaine public.</em></p>
+			
 		</div>
 		<div class="sidebar">
 			<?php include('logos_menus_verticaux.inc.php'); ?>
