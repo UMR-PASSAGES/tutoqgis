386dcfb33bfcf124b47754eb6cd68af6067a6de3
juliepierson <julie.pierson@cnrs.fr>
2018-10-29 12:51:22 +0100

    remove google analytics

--- head.inc.php
+++ head.inc.php
@@ -9,14 +9,4 @@
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="tutoqgis.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="tutoqgis_print.css" media="print">
-	<script>
-	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	    ga('create', 'UA-53969627-1', 'auto');
-	    ga('send', 'pageview');
-
-	</script>
 </head>
