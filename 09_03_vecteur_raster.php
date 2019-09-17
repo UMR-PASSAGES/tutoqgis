9da46db48348d706ed170dc9e4b7762efc6d5072
juliepierson <julie.pierson@cnrs.fr>
2019-09-17 12:14:03 +0200

    update QGIS 3 - spatial analysis

--- 09_03_vecteur_raster.php
+++ 09_03_vecteur_raster.php
@@ -26,79 +26,69 @@
 			<h3><a class="titre" id="IX31">Préparation des données : un seul SCR pour tous</a></h3>
 			
 				<div class="manip">
-					<p>Ouvrez un nouveau projet QGIS, ajoutez-y la couche créée en <a href="09_02_raster.php#IX23" >IX.2.3</a> <em class="data">pente_jamaique_JAD2001.tif</em>.</p>
-					<p>Ajoutez également la couche <em class="data">echantillons_jam</em> fournie dans le dossier <b>TutoQGIS_09_AnalyseSpat/donnees</b>.</p>
+					<p>Ouvrez un nouveau projet QGIS, ajoutez-y la couche <em class="data">srtm_21_09.tif</em>.</p>
+					<p>Ajoutez également la couche <em class="data"><a href="donnees/TutoQGIS_09_AnalyseSpat.zip">echantillons_jamaique_JAD2001</a></em> fournie dans le dossier <b>TutoQGIS_09_AnalyseSpat/donnees</b>. Cette couche correspond à la localisation de points d'études imaginaires.</p>
 					<div class="question">
 						<input type="checkbox" id="faq-1">
 						<p><label for="faq-1">Dans quel(s) SCR sont ces deux couches ?</label></p>
-						<p class="reponse">La couche <em class="data">pente_jamaique_JAD2001.tif</em> est en <b>JAD2001 EPSG:3448</b> et la couche <em class="data">echantillons_jam</em> en <b>WGS84 EPSG:4326</b>.</p>
+						<p class="reponse">On peut lire le SCR dans les propriétés de la couche, rubrique Source :</p>
+						<p class="reponse">srtm_21_09.tif &#8594; WGS84 EPSG:4326</p>
+						<p class="reponse">echantillons_jamaique_JAD2001 &#8594; JAD2001 EPSG:3448</p>
 					</div>
 					<div class="question">
 						<input type="checkbox" id="faq-2">
 						<p><label for="faq-2">Comment vous-y prendriez-vous pour passer ces deux couches dans le même SCR ?</label></p>
 						<p class="reponse">Il est possible soit de changer le SCR du raster, soit de changer le SCR du vecteur. Néanmoins, le temps de calcul est généralement moins long lorsqu'il s'agit de modifier le SCR d'une couche vecteur, en particulier pour une couche de points.</p>
 					</div>
-					<p>Sauvegardez la couche <em class="data">echantillons_jam</em> avec le SCR <b>JAD2001 EPSG:3448</b>, sous le nom <em class="data">echantillons_jam_JAD2001</em>. Référez-vous si nécessaire à la <a href="02_04_changer_systeme.php#II42" >partie II.4.2</a>. Ajoutez cette couche à QGIS.</p>
+					<p><a href="02_04_changer_systeme.php#II42" >Modifiez le SCR de la couche</a> <em class="data">echantillons_jamaique_JAD2001</em> vers le <b>WGS84 EPSG:4326</b>. Nommez la nouvelle couche <em class="data">echantillons_jamaique_WGS84</em>.</p>
+					<p>Supprimez de QGIS <em class="data">echantillons_jamaique_JAD2001</em>.</p>
+					<p>Votre projet QGIS doit contenir uniquement les 2 couches <em class="data">echantillons_jamaique_WGS84</em> et <em class="data">srtm_21_09</em>. Vérifiez qu'elles sont toutes deux en WGS84.</p>
 				</div>
+				<p>Nous avons vu qu'il n'est pas toujours nécessaire de travailler avec des couches dans le même SCR, certains outils acceptant de croiser 2 couches dans 2 SCR différents. Cependant, cela reste une bonne pratique et peut éviter des problèmes&nbsp;!</p>
 			
 			
-			<h3><a class="titre" id="IX32">Trouver l'outil : installation d'une extension</a></h3>
+			<h3><a class="titre" id="IX32">Attribuer à chaque point une valeur d'élévation</a></h3>
 			
-				<p>Comment faire pour donner à chacun des points sa valeur de pente ?</p>
-				<p>Il ne semble pas y avoir d'outil correspondant dans les menus vecteur et raster de QGIS. L'étape suivante est donc de faire une recherche parmi les extensions disponibles (ou bien dans un moteur de recherche, avec par exemple &#171; qgis raster point value &#187; comme mots clés).</p>
+				<p>Comment faire pour donner à chacun des points sa valeur d'élévation&nbsp;? Il faut pour cela attribuer à chaque point la valeur du pixel qui le recouvre.</p>
+				<p>Une recherche dans la boîte à outils, rubrique Analyse Raster, permet de découvrir l'outil <b>Prélever des valeurs raster</b> qui semble correspondre à ce que l'on cherche.</p>
 				
 				<div class="manip">
-					<p>Rendez-vous dans le menu <b>Extension &#8594; Installer/gérer les extensions</b> :</p>
-					<figure>
-						<a href="illustrations/tous/9_3_install_ext.png">
-							<img src="illustrations/tous/9_3_install_ext.png" alt="installation de l'extension point sampling tool" width="600" >
-						</a>
-					</figure>
-					<p>Dans la rubrique <b>En obtenir plus</b>, tapez <b>sampling</b> dans la partie filtre : plusieurs extensions sont trouvées. Lisez leurs descriptifs ; <b>Point sampling tool</b> semble correspondre à notre besoin. Installez cette extension.</p>
-					<p>Fermez la fenêtre du gestionnaire d'extensions.</p>
-				</div>
-					
-			<h3><a class="titre" id="IX33">Calcul de pente pour chacun des points d'une couche</a></h3>		
-					
-				<div class="manip">
-					<p><img class="icone" src="illustrations/tous/9_3_PST_icone.png" alt="icône Point sampling tool" >Point sampling tool est accessible soit via l'icône correspondante, soit via le menu
-						<a class="thumbnail_bottom" href="#thumb">Extension &#8594; Analyses &#8594; Point sampling tool
-							<span>
-								<img src="illustrations/tous/9_3_PST_menu.png" alt="Extension, Analyses, Point sampling tool" height="120" >
-							</span>
-						</a>
-					.</p>
-					
-					<p><b>Onglet General :</b></p>
-					<figure>
-						<a href="illustrations/tous/9_3_PST_fenetre_01.png">
-							<img src="illustrations/tous/9_3_PST_fenetre_01.png" alt="Fenêtre de point sampling tool, onglet general" width="440" >
+				    <p>
+				        <a class="thumbnail_bottom" href="#thumb">Boîte à outils &#8594; Analyse raster &#8594; Prélever des valeurs raster
+                        	<span>
+                        		<img src="illustrations/tous/9_3_prelev_raster_emplacement.png" alt="Emplacement de l'outil Prélever des valeurs raster dans la boîte à outils" width="80%" >
+                        	</span>
+                        </a>
+				    </p>
+				    <figure>
+						<a href="illustrations/tous/9_3_prelev_raster_fenetre.png" >
+							<img src="illustrations/tous/9_3_prelev_raster_fenetre.png" alt="Fenêtre de l'outil de prélèvement des valeurs raster" width="100%" >
 						</a>
 					</figure>
 					<ul>
-						<li class="espace"><b>Layer containing sampling points :</b> vérifiez que <em class="data">echantillons_jam_2001</em> soit bien sélectionnée dans la liste</li>
-						<li class="espace"><b>Layer with fields/bands to get values from :</b> il s'agit des couches dans les attributs seront repris dans la nouvelle couche. Faites bien attention à sélectionner <b>les deux couches</b>, pour garder à la fois les identifiants des points et leur pente</li>
-						<li class="espace"><b>Output point vector layer :</b> cliquez sur <b>Parcourir</b>, sélectionnez l'emplacement où la couche de points sera créée, tapez son nom : <em class="data">echantillons_jam_pente_JAD2001</em></li>
-						<li class="espace"><b>Add created layer to the TOC :</b> vérifiez que cette case soit cochée pour ajouter directement la couche créée à QGIS</li>
+					   <li class="espace">Couche de points en entrée : <em class="data">echantillons_jamaique_WGS84</em></li>
+					   <li class="espace">Raster layer to sample : <em class="data">srtm_21_09</em></li>
+					   <li class="espace">Préfixe de la colonne en sortie : vous pouvez taper par exemple <b>elev</b></li>
+					   <li class="espace">Laissez l'option de sortie par défaut, pour créer une couche temporaire</li>
 					</ul>
-					
-					<p>Avant de cliquer sur OK, passez à <b>l'onglet Champs :</b></p>
-					<figure>
-						<a href="illustrations/tous/9_3_PST_fenetre_02.png">
-							<img src="illustrations/tous/9_3_PST_fenetre_02.png" alt="Fenêtre de point sampling tool, onglet champs" width="440" >
-						</a>
-					</figure>
-					<p>Si vous avez bien sélectionné les deux couches dans l'onglet général, vous voyez ici les champs de ces deux couches : id pour les points, et la pente pour le raster. Vous pouvez renommer les champs qui seront créés : renommez par exemple <b>pente_jama</b> en <b>pente</b>.</p>
-					<p>Pour terminer, cliquez sur <b>OK</b>, fermez la fenêtre de l'extension.</p>
-					
-					<p>Vérifiez que la nouvelle couche contienne bien les mêmes 22 points qu'<em class="data">echantillons_jam_2001</em>.</p>
-					<p>Ouvrez sa table attributaire : les 2 champs <b>id</b> et <b>pente</b> sont présents. La pente est renseignée pour chacun des points.</p>
-					<figure>
-						<a href="illustrations/tous/9_3_points_pente_table.png">
-							<img src="illustrations/tous/9_3_points_pente_table.png" alt="table attributaire de la couche créée avec Point sampling tool, avec les 2 champs id et pente" width="170" >
+			     </div>
+			     <p>Ici, notre raster ne possède qu'une seule bande. Certains raster en possèdent plusieurs, c'est le cas par exemple des images satellites qui ont différentes bandes pour le vert, le rouge, l'infrarouge etc.</p>
+			     <p>Cet outil ajoutera autant de colonnes à la table de la couche de points que de bandes dans le raster. Ces champs auront en suffixe le numéro de bande (_01, _02...), auquel il est donc possible de rajouter un préfixe.</p>
+			     <p>Ici, le nouveau champ sera donc nommé elev_1.</p>
+			     <div class="manip">
+					<p>Cliquez sur <b>Exécuter</b>, la couche temporaire est ajoutée et se nomme <b>Sampled Points</b>. Elle possède les mêmes géométries que la couche de points en entrée, et une colonne de plus dans sa table.</p>
+					<p>Ouvrez sa table attributaire pour le vérifier : une colonne <b>elev_1</b> a été ajoutée, où l'élévation de chaque point est renseignée.</p>
+					 <figure>
+						<a href="illustrations/tous/9_3_table.png" >
+							<img src="illustrations/tous/9_3_table.png" alt="Table attributaire de la couche créée, avec la colonne elev_1" width="90%" >
 						</a>
 					</figure>
 				</div>
+				<p>Attention toutefois, la résolution des données d'élévation est de 90 mètres : si les points représentent une location précise par exemple au mètre près, l'élévation que nous leur avons attribué n'aura pas cette précision.</p>
+				<p>Nous avons vu ici un exemple très simple montrant qu'on peut croiser données vecteur et raster. Il est également possible de <b>transformer des données vecteur en raster (polygonisation) et inversement (rastérisation)</b>.</p>
+				<p>Par exemple, on peut vouloir rasteriser une série de couches vecteurs surfaciques pour pouvoir les additionner facilement, ou bien vectoriser une couche raster représentant des zones pour pouvoir y affecter des données attributaires...</p>
+				<p>Chaque type de données a une utilisation différente, mais il est utile de garder à l'esprit que les 2 existent&nbsp;!</p>
+					
 		
 				<br>
 				<a class="prec" href="09_02_raster.php">chapitre précédent</a>
