e36fd4ba8c16053312547fd3978c00f8dec29361
juliepierson <julie.pierson@cnrs.fr>
2019-09-24 16:36:08 +0200

    missing images + pdf

--- footer.inc.php
+++ footer.inc.php
@@ -1,7 +1,7 @@
-<div id="footer">
+<!-- <div id="footer">
 	<hr class="simple">
 	<a target="_blank" rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.fr">
 		<img alt="Licence Creative Commons" src="http://i.creativecommons.org/l/by/3.0/fr/80x15.png" >
 	</a>
 	Ce tutoriel est mis à disposition selon les termes de la <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.fr">Licence Creative Commons Attribution 4.0 International</a>
-</div>
+</div> -->
