87f44d112faed3d2568af6a7334aeaacc68d647f
Pierson Julie <pierson@2letg51.univ-brest.fr>
2021-02-05 16:06:47 +0100

    licence is by-sa

--- footer.inc.php
+++ footer.inc.php
@@ -1,7 +1,7 @@
 <div id="footer">
 	<hr class="simple">
-	<a target="_blank" rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.fr">
-		<img alt="Licence Creative Commons" src="http://i.creativecommons.org/l/by/3.0/fr/80x15.png" >
+	<a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.fr">
+		<img alt="Licence Creative Commons" src="illustrations/tous/by-sa.png" height="27" >
 	</a>
-	Ce tutoriel est mis à disposition selon les termes de la <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.fr">Licence Creative Commons Attribution 4.0 International</a>
+	Ce tutoriel est mis à disposition selon les termes de la <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/deed.fr">Licence Creative Commons Attribution 4.0 International</a>
 </div>
