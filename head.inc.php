df37c5fd4f38600761a593021c4191857ec4cb8e
Pierson Julie <pierson@2letg51.univ-brest.fr>
2021-02-05 16:09:46 +0100

    https instead of http for google fonts

--- head.inc.php
+++ head.inc.php
@@ -6,7 +6,7 @@
 	<title>Tutoriel QGIS Passages</title>
 	<meta name="description" content="Tutoriel QGIS">
 	<meta name="author" content="Pierson Julie" >
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="tutoqgis.css" media="screen">
 	<!--<link rel="stylesheet" type="text/css" href="tutoqgis_print.css" media="screen">-->
 </head>
