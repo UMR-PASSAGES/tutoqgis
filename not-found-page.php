5eba814745bdd987153abaf93cc1dbab4b4f37c7
juliepierson <julie.pierson@univ-brest.fr>
2023-01-20 14:08:04 +0100

    adding 404 page

---
+++ not-found-page.php
@@ -0,0 +1,39 @@
+<?php include('head.inc.php'); ?>
+
+<body>
+<div id="wrap">
+	<?php include('menus_horizontaux.inc.php'); ?>
+
+	<div id="container_main_sidebar">
+
+		<div class="backgrounds">
+			<div class="main"></div>
+			<div class="sidebar"></div>
+		</div>
+		
+		<div class="main">
+		
+			<h2>Désolé, cette page n'a pu être trouvée&nbsp;!</h2>
+			
+			 <p><a href="index.php">Revenir à la page d'accueil</a></p>
+			
+				
+		
+		</div>
+		
+		<div class="sidebar">
+			<?php include('logos_menus_verticaux.inc.php'); ?>
+			<?php include('menus_verticaux.inc.php'); ?>
+		</div>	
+		
+		<div id="notforprint" style="clear:both;"></div>
+	
+	</div>
+
+	<?php include('footer.inc.php'); ?>
+
+
+
+</div>
+</body>
+</html>
