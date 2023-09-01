adcca260cf09134018e79d1fbfa4759c64788ce8
juliepierson <julie.pierson@univ-brest.fr>
2023-09-01 17:14:32 +0200

    using bunny fonts instead of google fonts and using another arrow symbol for downloading, old one no longer working

--- head.inc.php
+++ head.inc.php
@@ -7,7 +7,7 @@
 	<meta name="description" content="Tutoriel QGIS">
 	<meta name="author" content="Pierson Julie" >
 	<link rel="icon" type="image/x-icon" href="illustrations/icon_letg.png">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.bunny.net/css?family=Open+Sans:400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="tutoqgis.css" media="screen">
 	<!--<link rel="stylesheet" type="text/css" href="tutoqgis_print.css" media="screen">-->
 </head>
