e7b1e2a86ddaad32fcc685e3aa98fd1ae58faa24
juliepierson <julie.pierson@univ-brest.fr>
2026-03-03 10:21:59 +0100

    titres de niveau 1 manquants ajoutés

--- en_savoir_plus.php
+++ en_savoir_plus.php
@@ -7,33 +7,34 @@
 	<div id="container_main_sidebar">
 
 		<div class="main">
-			<h2>En savoir plus</h2>
-			
-			    <h3>Sur QGIS</h3>
-			    
-			        <p>QGIS est un <a class="ext" target="_blank" href="https://fr.wikipedia.org/wiki/Logiciel_libre">logiciel libre</a> et peut donc être utilisé, étudié, modifié et redistribué sans restriction par la personne à qui il a été distribué.</p>
-			        <p>QGIS.org est une association dont la <a class="ext" target="_blank" href="https://www.qgis.org/fr/site/getinvolved/governance/governance.html">gouvernance</a> comprend différentes instances&nbsp;: bureau, comité de projet, groupes d'utilisateurs... Le fonctionnement de l'association, ses rapports annuels et financiers ainsi que bien d'autres informations sont <a class="ext" target="_blank" href="https://www.qgis.org/fr/site/getinvolved/governance/index.html">disponibles en ligne</a>.</p>
-			        <p>Toute personne peut contribuer à QGIS, même sans savoir programmer. Vous pouvez par exemple participer à la <a class="ext" target="_blank" href="https://docs.qgis.org/3.40/en/docs/documentation_guidelines/">rédaction de la documentation</a> ou bien à la <a class="ext" target="_blank" href="https://docs.qgis.org/3.40/en/docs/documentation_guidelines/do_translations.html">traduction de QGIS</a>. Le logiciel évoluant rapidement, ce travail est toujours nécessaire.</p>
-			        <p>Vous pouvez également <a class="ext" target="_blank" href="https://github.com/qgis/QGIS/issues">faire remonter les bugs</a> que vous rencontrez, en vous assurant que cela n'a pas déjà été fait. Il vous faudra alors détailler la version de QGIS utilisée, les circonstances dans lesquelles le bug se produit en joignant éventuellement une capture d'écran.</p>
-			        <p>Enfin, vous pouvez tout simplement <a class="ext" target="_blank" href="https://www.qgis.org/community/involve/#sustain--donatel">faire un don au projet</a>, ou bien acheter une tasse, un t-shirt, une gourde... via la <a class="ext" target="_blank" href="https://qgis.org/goodies/">boutique QGIS</a>.</p>
-			        <p>A l'instar des logiciels SIG propriétaires, QGIS dispose également d'un <a class="ext" target="_blank" href="https://www.qgis.org/resources/support/commercial-support/">support commercial</a> proposé par plusieurs entreprises. Il est également possible via ces entreprises de financer du développement pour améliorer ou ajouter certaines fonctionnalités.</p>
-			        <p>QGIS est <b>votre</b> logiciel. En y contribuant, au-delà de participer à son amélioration, vous apprendrez également des outils et méthodes collaboratifs qui seront un atout lors d'une recherche d'emploi&nbsp;!</p>
-			        
-			    
-			    <h3>Sur ce tutoriel</h3>
-			
-        			<p>Ce tutoriel a été initialement créé par le pôle Analyse et Représentation des Données de <a href="http://www.passages.cnrs.fr">l'UMR PASSAGES</a> (de même que les autres passionnants tutoriels sur la plate-forme <a href="http://ouvrir.passages.cnrs.fr">OUVRIR</a>). Il est aujourd'hui et depuis 2020 maintenu par <a href="https://letg.cnrs.fr/" >l'UMR LETG</a>.</p>
-        			<p>Il a bénéficié de relectures et conseils avisés de (par ordre alphabétique, UMR Passages sauf mention contraire)&nbsp;: Xavier Amelot, Véronique André-Lamat, Gaëlle Deletraz, Marina Duféal, Thierry Feuillet (Université de Caen Normandie - UMR IDEES), Grégoire Le Campion, Delphine Montagne, Claude Monteil (INP-ENSAT - Toulouse), Guilhem Mousselin, Matthieu Noucher, Olivier Pissoat, Jean-Michel Poux (Ministère de la Transition Écologique et Solidaire), Raphaël Schirmer, David Sheeren (INP-ENSAT - Toulouse). Merci à elles et eux&nbsp;!</p>
-        			
-        			<p>Ce tutoriel est diffusé sous <b>licence Creative Commons</b>&nbsp;: vous êtes autorisé à le partager et l'adapter, pour toute utilisation y compris commerciale, à condition de citer l'auteur (<em>UMR 5319 PASSAGES/UMR 6554 LETG, https://tutoqgis.cnrs.fr/</em>), et de redistribuer votre travail sous les mêmes conditions. Le texte complet est disponible ici&nbsp;: <a class="ext" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">http://creativecommons.org/licenses/by/4.0/legalcode</a></p>
-        			<p>Cela signifie donc que vous pouvez en faire ce que bon vous semble, y compris l'utiliser pour vos propres formations. Et dans ce cas, un petit message nous fait plaisir et nous encourage à continuer&nbsp;! Faites-nous part également de vos remarques, critiques et idées d'amélioration&nbsp;!</p>
-        
-        			<p><b>Pour tout contact&nbsp;:</b> envoyez un courriel à julie.pierson (at) cnrs.fr, ou bien <a class="ext" target="_blank" href="https://mapstodon.space/@sigomatique">@sigomatique</a> sur Mastodon (<a class="ext" target="_blank" href="https://mapstodon.space/">mapstodon.space</a>).</p>
-        			
-        			<p>Enfin, tout le code du tutoriel (HTML, CSS...) est <b>disponible sur GitLab</b>&nbsp;: <a class="ext" target="_blank" href="https://gitlab.in2p3.fr/letg/tutoqgis/-/tree/html" >https://gitlab.in2p3.fr/letg/tutoqgis/</a></p>
-        			
-        			<p><em>L'image du bandeau est tirée des archives de l'art américain et est disponible sur <a class="ext" target="_blank" href="http://commons.wikimedia.org/wiki/File:archives_of_American_Art_-_Guy_Maccoy_-_2905.jpg" >Wikimedia</a>. Cette image est dans le domaine public.</em></p>
-        			
+		
+  			<h1>En savoir plus</h1>
+  			
+  			    <h2>Sur QGIS</h2>
+  			    
+  			        <p>QGIS est un <a class="ext" target="_blank" href="https://fr.wikipedia.org/wiki/Logiciel_libre">logiciel libre</a> et peut donc être utilisé, étudié, modifié et redistribué sans restriction par la personne à qui il a été distribué.</p>
+  			        <p>QGIS.org est une association dont la <a class="ext" target="_blank" href="https://www.qgis.org/fr/site/getinvolved/governance/governance.html">gouvernance</a> comprend différentes instances&nbsp;: bureau, comité de projet, groupes d'utilisateurs... Le fonctionnement de l'association, ses rapports annuels et financiers ainsi que bien d'autres informations sont <a class="ext" target="_blank" href="https://www.qgis.org/fr/site/getinvolved/governance/index.html">disponibles en ligne</a>.</p>
+  			        <p>Toute personne peut contribuer à QGIS, même sans savoir programmer. Vous pouvez par exemple participer à la <a class="ext" target="_blank" href="https://docs.qgis.org/3.40/en/docs/documentation_guidelines/">rédaction de la documentation</a> ou bien à la <a class="ext" target="_blank" href="https://docs.qgis.org/3.40/en/docs/documentation_guidelines/do_translations.html">traduction de QGIS</a>. Le logiciel évoluant rapidement, ce travail est toujours nécessaire.</p>
+  			        <p>Vous pouvez également <a class="ext" target="_blank" href="https://github.com/qgis/QGIS/issues">faire remonter les bugs</a> que vous rencontrez, en vous assurant que cela n'a pas déjà été fait. Il vous faudra alors détailler la version de QGIS utilisée, les circonstances dans lesquelles le bug se produit en joignant éventuellement une capture d'écran.</p>
+  			        <p>Enfin, vous pouvez tout simplement <a class="ext" target="_blank" href="https://www.qgis.org/community/involve/#sustain--donatel">faire un don au projet</a>, ou bien acheter une tasse, un t-shirt, une gourde... via la <a class="ext" target="_blank" href="https://qgis.org/goodies/">boutique QGIS</a>.</p>
+  			        <p>A l'instar des logiciels SIG propriétaires, QGIS dispose également d'un <a class="ext" target="_blank" href="https://www.qgis.org/resources/support/commercial-support/">support commercial</a> proposé par plusieurs entreprises. Il est également possible via ces entreprises de financer du développement pour améliorer ou ajouter certaines fonctionnalités.</p>
+  			        <p>QGIS est <b>votre</b> logiciel. En y contribuant, au-delà de participer à son amélioration, vous apprendrez également des outils et méthodes collaboratifs qui seront un atout lors d'une recherche d'emploi&nbsp;!</p>
+  			        
+  			    
+  			    <h2>Sur ce tutoriel</h2>
+  			
+          			<p>Ce tutoriel a été initialement créé par le pôle Analyse et Représentation des Données de <a href="http://www.passages.cnrs.fr">l'UMR PASSAGES</a> (de même que les autres passionnants tutoriels sur la plate-forme <a href="http://ouvrir.passages.cnrs.fr">OUVRIR</a>). Il est aujourd'hui et depuis 2020 maintenu par <a href="https://letg.cnrs.fr/" >l'UMR LETG</a>.</p>
+          			<p>Il a bénéficié de relectures et conseils avisés de (par ordre alphabétique, UMR Passages sauf mention contraire)&nbsp;: Xavier Amelot, Véronique André-Lamat, Gaëlle Deletraz, Marina Duféal, Thierry Feuillet (Université de Caen Normandie - UMR IDEES), Grégoire Le Campion, Delphine Montagne, Claude Monteil (INP-ENSAT - Toulouse), Guilhem Mousselin, Matthieu Noucher, Olivier Pissoat, Jean-Michel Poux (Ministère de la Transition Écologique et Solidaire), Raphaël Schirmer, David Sheeren (INP-ENSAT - Toulouse). Merci à elles et eux&nbsp;!</p>
+          			
+          			<p>Ce tutoriel est diffusé sous <b>licence Creative Commons</b>&nbsp;: vous êtes autorisé à le partager et l'adapter, pour toute utilisation y compris commerciale, à condition de citer l'auteur (<em>UMR 5319 PASSAGES/UMR 6554 LETG, https://tutoqgis.cnrs.fr/</em>), et de redistribuer votre travail sous les mêmes conditions. Le texte complet est disponible ici&nbsp;: <a class="ext" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">http://creativecommons.org/licenses/by/4.0/legalcode</a></p>
+          			<p>Cela signifie donc que vous pouvez en faire ce que bon vous semble, y compris l'utiliser pour vos propres formations. Et dans ce cas, un petit message nous fait plaisir et nous encourage à continuer&nbsp;! Faites-nous part également de vos remarques, critiques et idées d'amélioration&nbsp;!</p>
+          
+          			<p><b>Pour tout contact&nbsp;:</b> envoyez un courriel à julie.pierson (at) cnrs.fr, ou bien <a class="ext" target="_blank" href="https://mapstodon.space/@sigomatique">@sigomatique</a> sur Mastodon (<a class="ext" target="_blank" href="https://mapstodon.space/">mapstodon.space</a>).</p>
+          			
+          			<p>Enfin, tout le code du tutoriel (HTML, CSS...) est <b>disponible sur GitLab</b>&nbsp;: <a class="ext" target="_blank" href="https://gitlab.in2p3.fr/letg/tutoqgis/-/tree/html" >https://gitlab.in2p3.fr/letg/tutoqgis/</a></p>
+          			
+          			<p><em>L'image du bandeau est tirée des archives de l'art américain et est disponible sur <a class="ext" target="_blank" href="http://commons.wikimedia.org/wiki/File:archives_of_American_Art_-_Guy_Maccoy_-_2905.jpg" >Wikimedia</a>. Cette image est dans le domaine public.</em></p>
+          			
 		</div>
 		<div class="sidebar">
 			<?php include('logos_menus_verticaux.inc.php');?>
--- telechargement.php
+++ telechargement.php
@@ -7,7 +7,8 @@
 	<div id="container_main_sidebar">
 	
 		<div class="main">
-			<h2>Téléchargement des données</h2>
+		
+			<h1>Téléchargement des données</h1>
 				<p>Les données nécessaires au tutoriel sont des données libres d'accès.</p>
 				<p>Chaque dossier contient&nbsp;:</p>
 				<ul>
@@ -17,34 +18,34 @@
 					<li class="espace">un sous-dossier <b>metadonnees</b>, avec les fichier de métadonnées. Si toutes les métadonnées sont disponibles en ligne, ce sous-dossier n'existe pas.</li>
 				</ul>
 				<p>Vous pouvez télécharger ici&nbsp;:</p>
-				<p><a download href="donnees/TutoQGIS_Donnees.zip">Toutes les données (280,2 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_01_PriseEnMain.zip">Données de la partie I&nbsp;: prise en main (13,1 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_02_Geodesie.zip">Données de la partie II&nbsp;: géodésie (2,8 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_03_RechercheDonnees.zip">Données de la partie III&nbsp;: recherche et ajout de données (47,6 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_04_Georef.zip">Données de la partie IV&nbsp;: géoréférencement (7,7 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_Donnees.zip">Toutes les données (582,2 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_01_PriseEnMain.zip">Données de la partie I&nbsp;: prise en main (13 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_02_Geodesie.zip">Données de la partie II&nbsp;: géodésie (3 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_03_RechercheDonnees.zip">Données de la partie III&nbsp;: recherche et ajout de données (93 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_04_Georef.zip">Données de la partie IV&nbsp;: géoréférencement (7,2 Mo)</a></p>
 				<p><a download href="donnees/TutoQGIS_05_Numerisation.zip">Données de la partie V&nbsp;: numérisation (20,2 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_06_Requetes.zip">Données de la partie VI&nbsp;: requêtes (8,9 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_07_Champs.zip">Données de la partie VII&nbsp;: calcul de champs (4,2 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_08_Jointures.zip">Données de la partie VIII&nbsp;: jointures (85,7 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_09_AnalyseSpat.zip">Données de la partie IX&nbsp;: analyse spatiale (29,7 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_10_Representation.zip">Données de la partie X&nbsp;: représentation et mise en page (59,7 Mo)</a></p>
-				<p><a download href="donnees/TutoQGIS_11_Automatisation.zip">Données de la partie XI&nbsp;: automatisation de traitements (0,6 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_06_Requetes.zip">Données de la partie VI&nbsp;: requêtes (8,5 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_07_Champs.zip">Données de la partie VII&nbsp;: calcul de champs (3,4 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_08_Jointures.zip">Données de la partie VIII&nbsp;: jointures (106,5 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_09_AnalyseSpat.zip">Données de la partie IX&nbsp;: analyse spatiale (35,7 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_10_Representation.zip">Données de la partie X&nbsp;: représentation et mise en page (53,9 Mo)</a></p>
+				<p><a download href="donnees/TutoQGIS_11_Automatisation.zip">Données de la partie XI&nbsp;: automatisation de traitements (11,9 Mo)</a></p>
 				
-			<h2>Téléchargement du tutoriel au format PDF</h2>
+			<h1>Téléchargement du tutoriel au format PDF</h1>
 				<p>Vous pouvez télécharger ici des PDF correspondant à la version en ligne du tutoriel QGIS. Ces PDF ont été générés grâce à l'outil <a class="ext" target="_blank" href="http://wkhtmltopdf.org/">wkhtmltopdf</a>.</p>
 				<p>Attention, ces PDF étant des exports de la version en ligne, les mises à jour sont moins régulières et toutes les fonctionnalités ne seront pas affichées.</p>
-				<p><a download href="exports_pdf/tutoqgis.pdf">Tout le tutoriel au format PDF (40,1 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_01_priseenmain.pdf">PDF de la partie I&nbsp;: prise en main (3 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_02_geodesie.pdf">PDF de la partie II&nbsp;: géodésie (2,3 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_03_recherche_donnees.pdf">PDF de la partie III&nbsp;: recherche et ajout de données (5,3 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_04_georeferencement.pdf">PDF de la partie IV&nbsp;: géoréférencement (5,1 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_05_numerisation.pdf">PDF de la partie V&nbsp;: numérisation (5,6 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_06_requetes.pdf">PDF de la partie VI&nbsp;: requêtes (3,4 Mo)</a> </p>
-				<p><a download href="exports_pdf/tutoqgis_07_calcul_champs.pdf">PDF de la partie VII&nbsp;: calcul de champs (1,5 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_08_jointures.pdf">PDF de la partie VIII&nbsp;: jointures (2,2 Mo)</a> </p>
-				<p><a download href="exports_pdf/tutoqgis_09_analyse_spatiale.pdf">PDF de la partie IX&nbsp;: analyse spatiale (6,9 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_10_miseenpage.pdf">PDF de la partie X&nbsp;: représentation et mise en page (4,9 Mo)</a></p>
-				<p><a download href="exports_pdf/tutoqgis_11_automatisation.pdf">PDF de la partie XI&nbsp;: automatisation de traitements (1,5 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis.pdf">Tout le tutoriel au format PDF (60,2 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_01_priseenmain.pdf">PDF de la partie I&nbsp;: prise en main (6,4 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_02_geodesie.pdf">PDF de la partie II&nbsp;: géodésie (4,7 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_03_recherche_donnees.pdf">PDF de la partie III&nbsp;: recherche et ajout de données (7,7 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_04_georeferencement.pdf">PDF de la partie IV&nbsp;: géoréférencement (4,3 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_05_numerisation.pdf">PDF de la partie V&nbsp;: numérisation (6,3 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_06_requetes.pdf">PDF de la partie VI&nbsp;: requêtes (4,1 Mo)</a> </p>
+				<p><a download href="exports_pdf/tutoqgis_07_calcul_champs.pdf">PDF de la partie VII&nbsp;: calcul de champs (2,6 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_08_jointures.pdf">PDF de la partie VIII&nbsp;: jointures (2,9 Mo)</a> </p>
+				<p><a download href="exports_pdf/tutoqgis_09_analyse_spatiale.pdf">PDF de la partie IX&nbsp;: analyse spatiale (12,9 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_10_miseenpage.pdf">PDF de la partie X&nbsp;: représentation et mise en page (7,5 Mo)</a></p>
+				<p><a download href="exports_pdf/tutoqgis_11_automatisation.pdf">PDF de la partie XI&nbsp;: automatisation de traitements (2,2 Mo)</a></p>
 
 			    <br>
 			    
